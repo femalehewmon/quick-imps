8b54fa2e2766677a5b53974535eacc75dd9d1f3b
Megan Van Welie <megan.van.welie@gmail.com>
2015-11-19 10:49:15 -0500

    print shortest path from graph_search BFS

--- algs/graph_search.py
+++ algs/graph_search.py
@@ -1,32 +1,36 @@
 from collections import defaultdict
 import Queue as queue
 
+DEBUG = False
+
 class Vertex:
     WHITE = "white"
     GREY = "grey"
     BLACK = "black"
     def __init__(self, name):
         self.name = name
-        self.c = Vertex.WHITE #color
-        self.p = None #predecessor
-        self.d = float("inf") #distance from source
+        self.c = Vertex.WHITE # color to be used in BFS
+        self.p = None # predecessor in graph
+        self.d = float("inf") # distance from source vertex
 
-    def info(self):
-        return "{}, {}".format(self.name, self.c)
+    def info(self, recurse=True):
+        info = "{}, {}".format(self.name, self.c)
+        if self.p is not None and recurse:
+            info += ", predecessor: {}".format(self.p.info(False))
+        return info
 
+# graph implementation using adj-lists
 class Graph:
     def __init__(self):
-        self.vertices = []
-        self.adj = defaultdict(set) #adj-list for all vertices
+        self.vertices = set() # ordered list of vertices
+        self.adj = defaultdict(set) # adj-list for all vertex edges
 
     def addEdge(self, u, v, undirected=False):
         self.adj[u].add(v)
         if undirected:
             self.adj[v].add(u)
-        if u not in self.vertices:
-            self.vertices.append(u)
-        if v not in self.vertices:
-            self.vertices.append(v)
+        self.vertices.add(u)
+        self.vertices.add(v)
 
     def info(self):
         print("Graph, {} vertices, {} adjMatrix".\
@@ -37,13 +41,15 @@ class Graph:
                 print("  {}".format(u.info()))
 
 
+# CLRS pg. 595
+# assumes that the input graph is represented using adj-lists
 def BFS(graph, s):
     # initialize graph for search
     for v in graph.vertices:
         v.c = Vertex.WHITE
         v.d = float("inf")
         v.p = None
-    graph.info()
+    if DEBUG: graph.info()
 
     # begin search
     s.c = Vertex.GREY
@@ -53,16 +59,28 @@ def BFS(graph, s):
     q.put(s)
     while not q.empty():
         u = q.get()
-        print("Searching edges off {}".format(u.name))
+        if DEBUG: print("Searching edges off {}".format(u.name))
         for v in graph.adj[u]:
-            print("    {}".format(v.name))
+            if DEBUG: print("    {}".format(v.name))
             if v.c == Vertex.WHITE:
                 v.c = Vertex.GREY
                 v.d = u.d + 1
                 v.p = u
                 q.put(v)
         u.c = Vertex.BLACK
-        graph.info()
+        if DEBUG: graph.info()
+
+# CLRS pg. 601
+# prints out the vertices on a shortest path from s to v
+# assuming that BFS has already computed a breadth-first tree
+def print_path(graph, s, v):
+    if v == s: #if v in graph.adj[s]:
+        print(s.info())
+    elif v.p == None:
+        print("No path from {} to {} exists".format(s.name, v.name))
+    else:
+        print_path(graph, s, v.p)
+        print(v.info())
 
 def main():
     r = Vertex('r')
@@ -87,5 +105,6 @@ def main():
     graph.addEdge(x, y, True)
 
     BFS(graph, s)
+    print_path(graph, s, v)
 
 main()
