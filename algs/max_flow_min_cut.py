f0a0ce3c84dfaa30329637cfa82e76b154af80fd
Megan Van Welie <mvanweli@cs.tufts.edu>
2016-05-26 12:46:18 -0400

    improvements on max flow/min cut

--- algs/max_flow_min_cut.py
+++ algs/max_flow_min_cut.py
@@ -1,10 +1,10 @@
 # reference, http://www.cse.unt.edu/~tarau/teaching/AnAlgo/Ford%E2%80%93Fulkerson%20algorithm.pdf
-import os
-import sys
+import Queue as queue
 
 DEBUG = False
 
-class Edge:
+
+class Edge(object):
     def __init__(self, u, v, capacity):
         self.u = u
         self.v = v
@@ -13,7 +13,8 @@ class Edge:
     def __str__(self):
         return "{0}->{1}, c={2}".format(self.u, self.v, self.c)
 
-class NetworkFlowGraph:
+
+class NetworkFlowGraph(object):
     def __init__(self):
         self.adj = {}
         self.flow = {}
@@ -73,8 +74,29 @@ class NetworkFlowGraph:
 
     # get augmenting path via breadth first search
     def get_shortest_augmenting_path(self):
-        # TODO: implement for EK algorithm!
-        pass
+        path = []
+        edge_path = {}
+        q = queue.Queue()
+        q.put(('source', None))
+        while not q.empty():
+            u = q.get()
+            if u[0] == 'sink':
+                # path to sink found, reconstruct path
+                prev_edge = u[1]
+                while prev_edge is not None:
+                    path.append(prev_edge)
+                    print "curr edge " + str(prev_edge) + " prev edge is " + str(edge_path[prev_edge])
+                    prev_edge = edge_path[prev_edge]
+                for e in path:
+                    print "  " + str(e) + " with remaining flow " + \
+                            str(self.residual_flow[e.u][e.v])
+                return path
+            for e in self.adj[u[0]]:
+                residual = self.residual_flow[e.u][e.v]
+                if residual > 0:
+                    q.put((e.v, e))
+                    edge_path[e] = u[1]
+        return path
 
     def get_total_flow(self, vertex='source'):
         total_flow = 0
@@ -88,6 +110,34 @@ class NetworkFlowGraph:
             if self.residual_flow[e.u][e.v] < min_cap:
                 min_cap = self.residual_flow[e.u][e.v]
         return min_cap
+
+
+class NetworkFlowGraphWithCapacityOnVertices(NetworkFlowGraph):
+    def __init__(self):
+        super(NetworkFlowGraphWithCapacityOnVertices, self).__init__()
+        self.buffer_vertices = {}
+
+    def add_vertex(self, vertex, capacity=float("inf")):
+        if vertex not in self.adj:
+            bf = 'buffer_' + vertex
+            self.buffer_vertices[vertex] = bf
+            super(NetworkFlowGraphWithCapacityOnVertices, self).\
+                    add_vertex(vertex)
+            super(NetworkFlowGraphWithCapacityOnVertices, self).\
+                    add_vertex(bf)
+            self.adj[vertex].append(Edge(bf, vertex, capacity))
+            self.flow[bf][vertex] = 0
+            self.residual_flow[bf][vertex] = capacity
+
+    def add_edge(self, u, v, _=''):
+        super(NetworkFlowGraphWithCapacityOnVertices, self).add_edge(
+                u, self.buffer_vertices[v], float("inf"))
+
+    def add_flow(self, edge, flow):
+        bf = self.buffer_vertices[edge.v]
+        self.flow[bf][edge.v] += flow
+        self.residual_flow[bf][edge.v] -= flow
+
 
 def max_flow_ford_fulkerson(multigraph):
     multigraph.reset_flow()
@@ -103,12 +153,13 @@ def max_flow_ford_fulkerson(multigraph):
 def max_flow_edmonds_karp(multigraph):
     multigraph.reset_flow()
     path = multigraph.get_augmenting_path()
-    while path is not None:
+    while len(path) > 0:
         min_c = multigraph.get_min_capacity(path)
         if DEBUG: print "adding flow " + str(min_c)
         for e in path:
             multigraph.add_flow(e, min_c)
         path = multigraph.get_shortest_augmenting_path()
+        print "path is " + str(path)
     return multigraph.get_total_flow()
 
 def main():
@@ -145,6 +196,50 @@ def main():
     print "Max flow from FF: {0}".format(max_flow)
     #max_flow = max_flow_edmonds_karp(network)
     #print "Max flow from EK: {0}".format(max_flow)
+
+    vc_network = NetworkFlowGraphWithCapacityOnVertices()
+    vc_network.add_source('x1')
+    vc_network.add_source('x2')
+    vc_network.add_sink('y1')
+    vc_network.add_sink('y2')
+    vc_network.add_sink('y3')
+
+    vc_network.add_vertex('a', 7)
+    vc_network.add_vertex('b', 18)
+    vc_network.add_vertex('c', 22)
+    vc_network.add_vertex('d', 16)
+    vc_network.add_vertex('e', 24)
+    vc_network.add_vertex('y1', 6)
+    vc_network.add_vertex('y2', 19)
+    vc_network.add_vertex('y3', 19)
+
+    vc_network.add_edge('x1', 'a')
+    vc_network.add_edge('x1', 'b')
+    vc_network.add_edge('x1', 'c')
+    vc_network.add_edge('x2', 'a')
+    vc_network.add_edge('x2', 'c')
+    vc_network.add_edge('x2', 'd')
+    vc_network.add_edge('a', 'y1')
+    vc_network.add_edge('a', 'e')
+    vc_network.add_edge('a', 'c')
+    vc_network.add_edge('b', 'y1')
+    vc_network.add_edge('b', 'y2')
+    vc_network.add_edge('b', 'a')
+    vc_network.add_edge('c', 'e')
+    vc_network.add_edge('c', 'y3')
+    vc_network.add_edge('d', 'y2')
+    vc_network.add_edge('d', 'y3')
+    vc_network.add_edge('d', 'b')
+    vc_network.add_edge('d', 'c')
+    vc_network.add_edge('e', 'b')
+    vc_network.add_edge('e', 'd')
+    print str(vc_network.adj)
+    print str(vc_network.flow)
+    print str(vc_network.residual_flow)
+
+
+    max_flow = max_flow_ford_fulkerson(vc_network)
+    print "Max flow from vertex capacity network FF: {0}".format(max_flow)
 
 main()
 
